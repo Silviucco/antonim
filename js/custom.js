2de19e03808d6a4b13b555a5a319315a6c02c239
SilviuCo <silviu.cozmiuc@supremedia.com>
2023-02-09 17:30:33 +0200

    update

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,9 @@
-$(function() {
+$(function () {
     'use strict'; // Start of use strict
 
-    $('.odometer').appear(function(e) {
+    $('.odometer').appear(function (e) {
         var odo = $(".odometer");
-        odo.each(function() {
+        odo.each(function () {
             var countNumber = $(this).attr("data-count");
             $(this).html(countNumber);
         });
@@ -23,14 +23,14 @@ $(function() {
      Gallery Popup JS
     ---------------------------- */
 
-    $('.gallery-popup').each(function() {
-		$(this).magnificPopup({
-			delegate: 'a', 
-			type: 'image',
-			gallery: {
-			  enabled:true
-			}
-		});
+    $('.gallery-popup').each(function () {
+        $(this).magnificPopup({
+            delegate: 'a',
+            type: 'image',
+            gallery: {
+                enabled: true
+            }
+        });
     });
 
     /*--------------------------
@@ -39,19 +39,19 @@ $(function() {
 
     function countdown() {
         $('.countdown').each(function () {
-          var $this = $(this),
-            finalDate = $(this).data('countdown');
-          $this.countdown(finalDate, function (event) {
-            $(this).html(event.strftime('<li><span>%-D</span><p>Days</p></li>' + '<li><span>%-H</span><p>Hours</p></li>' + '<li><span>%-M</span><p>Minutes</p></li>' + '<li><span>%S</span><p>Seconds</p></li>'));
-          });
+            var $this = $(this),
+                finalDate = $(this).data('countdown');
+            $this.countdown(finalDate, function (event) {
+                $(this).html(event.strftime('<li><span>%-D</span><p>Days</p></li>' + '<li><span>%-H</span><p>Hours</p></li>' + '<li><span>%-M</span><p>Minutes</p></li>' + '<li><span>%S</span><p>Seconds</p></li>'));
+            });
         });
-      };
-    
+    };
+
     /*--------------------------
        FAQ Accordion
     ---------------------------- */
 
-    $('.accordion').find('.accordion-title').on('click', function() {
+    $('.accordion').find('.accordion-title').on('click', function () {
         $(this).toggleClass('active');
         $(this).next().slideToggle('fast');
         $('.accordion-content').not($(this).next()).slideUp('fast');
@@ -62,23 +62,23 @@ $(function() {
      Tabs Box
     ---------------------------- */
 
-	if($('.tabs-box').length){
-		$('.tabs-box .tab-buttons .tab-btn').on('click', function(e) {
-			e.preventDefault();
-			var target = $($(this).attr('data-tab'));
-			
-			if ($(target).is(':visible')){
-				return false;
-			}else{
-				target.parents('.tabs-box').find('.tab-buttons').find('.tab-btn').removeClass('active-btn');
-				$(this).addClass('active-btn');
-				target.parents('.tabs-box').find('.tabs-content').find('.tab').fadeOut(0);
-				target.parents('.tabs-box').find('.tabs-content').find('.tab').removeClass('active-tab animated fadeIn');
-				$(target).fadeIn(300);
-				$(target).addClass('active-tab animated fadeIn');
-			}
-		});
-	}
+    if ($('.tabs-box').length) {
+        $('.tabs-box .tab-buttons .tab-btn').on('click', function (e) {
+            e.preventDefault();
+            var target = $($(this).attr('data-tab'));
+
+            if ($(target).is(':visible')) {
+                return false;
+            } else {
+                target.parents('.tabs-box').find('.tab-buttons').find('.tab-btn').removeClass('active-btn');
+                $(this).addClass('active-btn');
+                target.parents('.tabs-box').find('.tabs-content').find('.tab').fadeOut(0);
+                target.parents('.tabs-box').find('.tabs-content').find('.tab').removeClass('active-tab animated fadeIn');
+                $(target).fadeIn(300);
+                $(target).addClass('active-tab animated fadeIn');
+            }
+        });
+    }
 
     /*--------------------------
      Partners
@@ -89,24 +89,24 @@ $(function() {
         autoplay: true,
         smartSpeed: 900,
         autoplayTimeout: 3000,
-        responsive:{
-            0:{
-                items:1
+        responsive: {
+            0: {
+                items: 1
             },
-            600:{
-                items:1
+            600: {
+                items: 1
             },
-			700:{
-                items:3
+            700: {
+                items: 3
             },
-            1000:{
-                items:4
+            1000: {
+                items: 4
             },
-			1300:{
-                items:5
+            1300: {
+                items: 5
             },
-            1600:{
-                items:5
+            1600: {
+                items: 5
             }
         }
     });
@@ -115,25 +115,25 @@ $(function() {
         Testimonials Sec
     ------------------------------------------------------------------*/
     $('.test-active').owlCarousel({
-        loop:true,
-        margin:30,
-        items:1,
-        navText:['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
-        nav:false,
-        dots:false,
-        responsive:{
-            0:{
-                items:1,
+        loop: true,
+        margin: 30,
+        items: 1,
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+        nav: false,
+        dots: false,
+        responsive: {
+            0: {
+                items: 1,
                 margin: 0
             },
-            767:{
-                items:2
+            767: {
+                items: 2
             },
-            992:{
-                items:2
+            992: {
+                items: 2
             },
-            1200:{
-                items:3
+            1200: {
+                items: 3
             }
         }
     });
@@ -141,99 +141,91 @@ $(function() {
     /*--------------------------
         Sticky
     ---------------------------- */
-    jQuery(window).on('scroll', function() {
-    if ($(this).scrollTop() > 50) {
-      $('header').addClass("sticky");
-    } else {
-      $('header').removeClass("sticky");
-    }
+    jQuery(window).on('scroll', function () {
+        if ($(this).scrollTop() > 50) {
+            $('header').addClass("sticky");
+        } else {
+            $('header').removeClass("sticky");
+        }
 
-    //Check to see if the window is top if not then display button
-    if ($(this).scrollTop() > 400) {
-      $('.scrollToTop').addClass('show');
-    } else {
-      $('.scrollToTop').removeClass('show');
-    }
-});
+        //Check to see if the window is top if not then display button
+        if ($(this).scrollTop() > 400) {
+            $('.scrollToTop').addClass('show');
+        } else {
+            $('.scrollToTop').removeClass('show');
+        }
+    });
 
     /*--------------------------
         Slider
     ---------------------------- */
 
     function mainSlider() {
-	var BasicSlider = $('.slider-active');
-	BasicSlider.on('init', function (e, slick) {
-		var $firstAnimatingElements = $('.single-slider:first-child').find('[data-animation]');
-		doAnimations($firstAnimatingElements);
-	});
-	BasicSlider.on('beforeChange', function (e, slick, currentSlide, nextSlide) {
-		var $animatingElements = $('.single-slider[data-slick-index="' + nextSlide + '"]').find('[data-animation]');
-		doAnimations($animatingElements);
-	});
-	BasicSlider.slick({
-		autoplay: true,
-		autoplaySpeed: 10000,
-		dots:true ,
-		fade: true,
-		arrows: false,
-		prevArrow: '<button type="button" class="slick-prev"><i class="icon dripicons-chevron-left"></i></button>',
-		nextArrow: '<button type="button" class="slick-next"><i class="icon dripicons-chevron-right"></i></button>',
-		responsive: [
-			{ breakpoint: 1200, settings: { dots: false, arrows: false } }
-		]
-	});
+        var BasicSlider = $('.slider-active');
+        BasicSlider.on('init', function (e, slick) {
+            var $firstAnimatingElements = $('.single-slider:first-child').find('[data-animation]');
+            doAnimations($firstAnimatingElements);
+        });
+        BasicSlider.on('beforeChange', function (e, slick, currentSlide, nextSlide) {
+            var $animatingElements = $('.single-slider[data-slick-index="' + nextSlide + '"]').find('[data-animation]');
+            doAnimations($animatingElements);
+        });
+        BasicSlider.slick({
+            autoplay: true,
+            autoplaySpeed: 10000,
+            dots: true,
+            fade: true,
+            arrows: false,
+            prevArrow: '<button type="button" class="slick-prev"><i class="icon dripicons-chevron-left"></i></button>',
+            nextArrow: '<button type="button" class="slick-next"><i class="icon dripicons-chevron-right"></i></button>',
+            responsive: [
+                { breakpoint: 1200, settings: { dots: false, arrows: false } }
+            ]
+        });
 
-	function doAnimations(elements) {
-		var animationEndEvents = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-		elements.each(function () {
-			var $this = $(this);
-			var $animationDelay = $this.data('delay');
-			var $animationType = 'animated ' + $this.data('animation');
-			$this.css({
-				'animation-delay': $animationDelay,
-				'-webkit-animation-delay': $animationDelay
-			});
-			$this.addClass($animationType).one(animationEndEvents, function () {
-				$this.removeClass($animationType);
-			});
-		});
-	}
-}
-mainSlider();
+        function doAnimations(elements) {
+            var animationEndEvents = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+            elements.each(function () {
+                var $this = $(this);
+                var $animationDelay = $this.data('delay');
+                var $animationType = 'animated ' + $this.data('animation');
+                $this.css({
+                    'animation-delay': $animationDelay,
+                    '-webkit-animation-delay': $animationDelay
+                });
+                $this.addClass($animationType).one(animationEndEvents, function () {
+                    $this.removeClass($animationType);
+                });
+            });
+        }
+    }
+    mainSlider();
 
- 
-	/*------------------------------------------------------------------
+
+    /*------------------------------------------------------------------
         Year
     ------------------------------------------------------------------*/
-	$(function(){
-    var theYear = new Date().getFullYear();
-    $('#year').html(theYear);
-	});
-	
-	/*------------------------------------------------------------------
-       Mobile Menu Active
-    ------------------------------------------------------------------*/
-     jQuery('.mobile-menu-active').meanmenu({
-    meanMenuContainer: '.mobile-menu-container',
-    meanScreenWidth: "993"
-  });
+    $(function () {
+        var theYear = new Date().getFullYear();
+        $('#year').html(theYear);
+    });
 
 });
 
 /*------------------------------------------------------------------
- Loader 
+ Loader
 ------------------------------------------------------------------*/
-jQuery(window).on("load scroll", function() {
+jQuery(window).on("load scroll", function () {
     'use strict'; // Start of use strict
-    // Loader 
-     $('#dvLoading').fadeOut('slow', function () {
-            $(this).remove();
-        });
-	$('.google-map').on('click', function() {
-            $('.google-map').find('iframe').css("pointer-events", "auto");
-        });
-    //Animation Numbers	 
-    jQuery('.animateNumber').each(function() {
+    // Loader
+    $('#dvLoading').fadeOut('slow', function () {
+        $(this).remove();
+    });
+    $('.google-map').on('click', function () {
+        $('.google-map').find('iframe').css("pointer-events", "auto");
+    });
+    //Animation Numbers
+    jQuery('.animateNumber').each(function () {
         var num = jQuery(this).attr('data-num');
         var top = jQuery(document).scrollTop() + (jQuery(window).height());
         var pos_top = jQuery(this).offset().top;
@@ -243,12 +235,12 @@ jQuery(window).on("load scroll", function() {
             }, 2000);
         }
     });
-	  
+
 });
-	/*------------------------------------------------------------------
-    FAQ
-    ------------------------------------------------------------------*/
-    $('.panel-heading a').on('click', function() {
-        $('.panel-heading').removeClass('active');
-        $(this).parents('.panel-heading').addClass('active');
-    });
+/*------------------------------------------------------------------
+FAQ
+------------------------------------------------------------------*/
+$('.panel-heading a').on('click', function () {
+    $('.panel-heading').removeClass('active');
+    $(this).parents('.panel-heading').addClass('active');
+});
